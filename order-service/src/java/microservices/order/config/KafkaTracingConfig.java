d9789fdc760312d279bc626ff44ae1a285106d5f
Saul_QA <testst762@gmail.com>
2026-07-05 18:43:46 +0200

    Update KafkaTracingConfig.java

--- order-service/src/java/microservices/order/config/KafkaTracingConfig.java
+++ order-service/src/java/microservices/order/config/KafkaTracingConfig.java
@@ -1,4 +1,4 @@
-package com.saul.microservices.order.config;
+package microservices.order.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
