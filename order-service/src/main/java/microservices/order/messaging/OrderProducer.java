e74e09b6bcb5175726f31917e6b8430aba771984
Saul_QA <testst762@gmail.com>
2026-07-05 22:16:26 +0200

    OrderProducer

--- order-service/src/main/java/microservices/order/messaging/OrderProducer.java
+++ order-service/src/main/java/microservices/order/messaging/OrderProducer.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
-
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
